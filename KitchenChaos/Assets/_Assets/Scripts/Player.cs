1837c69a19afe225c3488d5e5aaaadd44b02d2fc
Jeff-OwensIyalekhue <j.oiyalekhue@hotmail.de>
2023-06-04 00:47:18 +0200

    Player Collision

--- KitchenChaos/Assets/_Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/_Assets/Scripts/Player.cs
@@ -16,13 +16,38 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * movementSpeed * Time.deltaTime;
+
+        float moveDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playewrHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playewrHeight, playerRadius, moveDir, moveDistance); 
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playewrHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playewrHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove)
+                    moveDir = moveDirZ;
+            }
+        }
+        
+        if (canMove) 
+            transform.position += moveDir * moveDistance;
 
         isWalking = moveDir != Vector3.zero;
 
-        float rotationSpeed = 10f;
-
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+        float rotationSpeed = 15f;
+        if (isWalking)
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 
     public bool IsWalking()
